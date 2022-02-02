bdf1cc15785db8d2b36b010bdc156211abdfa482
lilyclements <lclements1004@outlook.com>
2022-02-02 20:54:25 +0000

    Update test-airplane_emissions.R

--- tests/testthat/test-airplane_emissions.R
+++ tests/testthat/test-airplane_emissions.R
@@ -7,6 +7,9 @@ library(carbonr)
 data("airports")
 airports <- airports %>% dplyr::select(c(Name, City, IATA))
 
-test_that("correct calculations when you add people/trips", {
+test_that("correct calculations when you edit options on class and round trip", {
+  expect_equal(4*airplane_emissions(from = "BRS", to = "LHR"), airplane_emissions(from = "BRS", to = "LHR", class = "premium"))
   expect_equal(2*airplane_emissions(from = "LAX", to = "LHR"), airplane_emissions(from = "LAX", to = "LHR", round_trip = TRUE))
+  expect_equal(8*airplane_emissions(from = "LAX", to = "LHR"), airplane_emissions(from = "LAX", to = "LHR", round_trip = TRUE, class = "first"))
 })
+
