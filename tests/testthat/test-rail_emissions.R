e154b443567b91099d2ead15dfe4dc63c4dd07d1
lilyclements <lclements1004@outlook.com>
2022-02-02 21:07:49 +0000

    Create test-rail_emissions.R

---
+++ tests/testthat/test-rail_emissions.R
@@ -0,0 +1,7 @@
+context("rail emissions output")
+
+library(carbonr)
+
+test_that("correct calculations when you edit options on times and round trip", {
+  expect_equal(4*rail_emissions("Southampton Central", "Manchester Piccadilly"), rail_emissions("Southampton Central", "Manchester Piccadilly", round_trip = TRUE, times_journey = 2))
+})
