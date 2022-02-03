88ee42378e7da811d76fddfce94e619f1e1cc2ea
lilyclements <lclements1004@outlook.com>
2022-02-03 17:40:11 +0000

    hotel emissions test

---
+++ tests/testthat/test-hotel_emissions.R
@@ -0,0 +1,9 @@
+context("hotel emissions output")
+
+library(carbonr)
+
+test_that("calculations are correct when you edit options on hotel emissions", {
+  expect_equal(4*hotel_emissions(location = "UK"), hotel_emissions(location = "UK", nights = 4))
+  expect_equal(16*hotel_emissions(location = "UK"), hotel_emissions(location = "UK", nights = 4, rooms = 4))
+})
+
