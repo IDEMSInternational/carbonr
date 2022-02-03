8cac8efed7d8dce5a6827e7f9995905622de6955
lilyclements <lclements1004@outlook.com>
2022-02-03 00:23:13 +0000

    Update test-secondary_emissions.R

--- tests/testthat/test-secondary_emissions.R
+++ tests/testthat/test-secondary_emissions.R
@@ -2,6 +2,6 @@ context("secondary emissions output")
 
 library(carbonr)
 
-test_that("correct calculations when you edit frequency", {
+test_that("correct calculations when editing time parameter", {
   expect_equal(secondary_emissions(item = "IT equipment", cost = 1000), 52*secondary_emissions(item = "IT equipment", cost = 1000, time = "per year"))
 })
