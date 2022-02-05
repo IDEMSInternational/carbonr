f7ffa075629dddf03d01d6f08b543d5e3c64bce7
lilyclements <lclements1004@outlook.com>
2022-02-05 21:42:06 +0000

    Update test-office_emissions.R

--- tests/testthat/test-office_emissions.R
+++ tests/testthat/test-office_emissions.R
@@ -3,3 +3,7 @@ library(carbonr)
 test_that("correct calculations when editing number of people", {
   expect_equal(10*office_emissions(), office_emissions(num_people = 10))
 })
+
+test_that("correct calculations when specify is TRUE", {
+  expect_equal(office_emissions(specify = TRUE), 0)
+})
