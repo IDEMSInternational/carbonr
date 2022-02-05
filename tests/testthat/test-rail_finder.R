eaf36e2749f6d7c033c67ef50d018f32f89f13ce
lilyclements <lclements1004@outlook.com>
2022-02-05 21:44:06 +0000

    Update test-rail_finder.R

--- tests/testthat/test-rail_finder.R
+++ tests/testthat/test-rail_finder.R
@@ -11,6 +11,6 @@ test_that("correct output", {
   expect_equal(finder_typo$district, "Coventry")
 })
 
-test_that("works with multiple entries"){
+test_that("works with multiple entries", {
   expect_equal(rail_finder(region = "West Midlands", district = "Coventry", county = "West Midlands")$station = "Coventry")
 })
