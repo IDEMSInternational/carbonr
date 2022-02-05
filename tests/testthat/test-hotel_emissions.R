41c3ad600240f5f03a3265d8ffd4d0512dc56eea
lilyclements <lclements1004@outlook.com>
2022-02-05 23:01:59 +0000

    Update test-hotel_emissions.R

--- tests/testthat/test-hotel_emissions.R
+++ tests/testthat/test-hotel_emissions.R
@@ -7,3 +7,6 @@ test_that("calculations are correct when you edit options on hotel emissions", {
   expect_equal(16*hotel_emissions(location = "UK"), hotel_emissions(location = "UK", nights = 4, rooms = 4))
 })
 
+test_that("error function for incorrect locations", {
+  expect_error(hotel_emissions(location = "uk"))
+})
