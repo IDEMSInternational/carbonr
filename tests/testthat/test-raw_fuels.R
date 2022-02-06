707ae1d74465ef8e0503e2e231eb67da7797838a
lilyclements <lclements1004@outlook.com>
2022-02-06 18:11:08 +0000

    raw_fuels tests and minor edits

--- tests/testthat/test-raw_fuels.R
+++ tests/testthat/test-raw_fuels.R
@@ -1,9 +1,10 @@
 library(carbonr)
 
-#test_that("correct calculations when editing number of people", {
-#  expect_equal(10*office_emissions(), office_emissions(num_people = 10))
-#})
+test_that("correct calculations when editing number of people", {
+  expect_equal(10*raw_fuels(electricity_kwh = 10), raw_fuels(electricity_kwh = 10, num_people = 10))
+})
 
-#test_that("correct calculations when specify is TRUE", {
-#  expect_equal(office_emissions(specify = TRUE, electricity_kwh = 0), 0)
-#})
+test_that("changing units value alters calculation", {
+  expect_gt(raw_fuels(electricity_kwh = 0, propane = 100, propane_units = "tonnes"), raw_fuels(electricity_kwh = 0, propane = 100))
+  expect_gt(raw_fuels(electricity_kwh = 0, butane = 100, butane_units = "tonnes"), raw_fuels(electricity_kwh = 0, butane = 100))
+})
