67cc1159d761c422fac08897ae3454c6c04b5d63
lilyclements <lclements1004@outlook.com>
2022-02-07 22:40:27 +0000

    Update test-airplane_emissions.R

--- tests/testthat/test-airplane_emissions.R
+++ tests/testthat/test-airplane_emissions.R
@@ -13,10 +13,15 @@ test_that("correct calculations when you edit options on class and round trip", 
   expect_equal(8*airplane_emissions(from = "LAX", to = "LHR"), airplane_emissions(from = "LAX", to = "LHR", round_trip = TRUE, class = "first"))
 })
 
-
 # checking errors
 test_that("check an error arises for typos", {
   expect_error(airplane_emissions(from = "NBO", to = "LHRR"))
   expect_error(airplane_emissions(from = "Nairobi Airport", to = "LHR"))
   expect_error(airplane_emissions(from = "NBO", to = "LHR", via = ""))
+  expect_error(airplane_emissions(from = "NBO", to = "LHR", via = "AMMS"))
+})
+
+# via option
+test_that("via option", {
+  expect_gt(airplane_emissions(from = "NBO", to = "LHR", via = "AMS"), airplane_emissions(from = "NBO", to = "LHR"))
 })
