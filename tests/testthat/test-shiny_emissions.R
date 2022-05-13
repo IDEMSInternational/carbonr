f226d65c8e873b3c514ec13bb5569f5e403a7b59
lilyclements <lily@idems.international>
2022-05-13 16:26:09 +0100

    unit tests - shiny app

--- tests/testthat/test-shiny_emissions.R
+++ tests/testthat/test-shiny_emissions.R
@@ -1,5 +1,7 @@
 context("testing shiny app")
 
+# Expand on unit testing for shiny app
+
 test_that("returned object is shiny.appobj", {
   expect_equal(class(shiny_emissions()), c("shiny.appobj"))
 })
