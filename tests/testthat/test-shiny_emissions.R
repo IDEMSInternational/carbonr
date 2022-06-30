aa18b00a107bee595c5eb6bc82058c33b1a8d17d
lilyclements <lily@idems.international>
2022-06-30 09:03:06 +0100

    remove app

--- tests/testthat/test-shiny_emissions.R
+++
@@ -1,7 +0,0 @@
-context("testing shiny app")
-
-# Expand on unit testing for shiny app
-
-test_that("returned object is shiny.appobj", {
-  expect_equal(class(shiny_emissions()), c("shiny.appobj"))
-})
