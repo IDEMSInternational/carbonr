cfa5e5f2a6f0e3dd1937b6baa1722c80feac18b0
lilyclements <lily@idems.international>
2023-06-11 15:38:06 +0100

    testthat update

--- tests/testthat/tests-industrial_emissions.R
+++ tests/testthat/tests-industrial_emissions.R
@@ -1,11 +1,11 @@
 library(carbonr)
 
 test_that("correct calculations for industrial vs solo", {
-  expect_equal(paper_emissions(paper = 100, paper_WD = 10, units = "kg"), industrial_emissions(paper = 100, paper_WD = 10, units = "kg"))
-  expect_equal(plastic_emissions(PET = 100, PET_WD = 10, units = "kg"), industrial_emissions(PET = 100, PET_WD = 10, units = "kg"))
-  expect_equal(metal_emissions(aluminuim_cans = 100, aluminuim_cans_WD = 10, units = "kg"), industrial_emissions(aluminuim_cans = 100, aluminuim_cans_WD = 10, units = "kg"))
-  expect_equal(electrical_emissions(alkaline_batteries = 100, alkaline_batteries_WD = 10, units = "kg"), industrial_emissions(alkaline_batteries = 100, alkaline_batteries_WD = 10, units = "kg"))
-  expect_equal(construction_emissions(metals = 100, metals_WD = 10, units = "kg"), industrial_emissions(metals = 100, metals_WD = 10, units = "kg"))
+  expect_equal(paper_emissions(paper = 100, paper_WD = 10, units = "kg"), material_emissions(paper = 100, paper_WD = 10, paper_units = "kg"))
+  expect_equal(plastic_emissions(PET = 100, PET_WD = 10, units = "kg"), material_emissions(PET = 100, PET_WD = 10, plastic_units = "kg"))
+  expect_equal(metal_emissions(aluminuim_cans = 100, aluminuim_cans_WD = 10, units = "kg"), material_emissions(aluminuim_cans = 100, aluminuim_cans_WD = 10, metal_units = "kg"))
+  expect_equal(electrical_emissions(alkaline_batteries = 100, alkaline_batteries_WD = 10, units = "kg"), material_emissions(alkaline_batteries = 100, alkaline_batteries_WD = 10, electrical_units = "kg"))
+  expect_equal(construction_emissions(metals = 100, metals_WD = 10, units = "kg"), material_emissions(metals = 100, metals_WD = 10, metal_units = "kg"))
 })
 
 test_that("changing units value alters calculation", {
