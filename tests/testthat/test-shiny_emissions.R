b0cdf32e3821a8a040e33953723c54020493fce2
lilyclements <lily@idems.international>
2024-05-01 12:43:00 +0100

    shiny_emissions testthat update

--- tests/testthat/test-shiny_emissions.R
+++ tests/testthat/test-shiny_emissions.R
@@ -1,7 +1,15 @@
 context("testing shiny emissions app")
 
-# Expand on unit testing for shiny app
+# Helper function to check if the returned value is a Shiny app
+is_shiny_app <- function(x) {
+  inherits(x, "shiny.appobj")
+}
 
-test_that("returned object is shiny.appobj", {
-  expect_equal(class(shiny_emissions()), c("shiny.appobj"))
+# Test to ensure that shiny_emissions function executes and returns a Shiny app
+test_that("shiny_emissions launches a Shiny app", {
+  # Call the shiny_emissions function
+  app <- shiny_emissions()
+  
+  # Check if the output is a Shiny app object
+  expect_true(is_shiny_app(app))
 })
