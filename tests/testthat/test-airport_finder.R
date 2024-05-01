9e697be83425aa2b6cf799d32e45af118301e966
lilyclements <lily@idems.international>
2024-05-01 13:01:24 +0100

    airport_finder test fix

--- tests/testthat/test-airport_finder.R
+++ tests/testthat/test-airport_finder.R
@@ -10,5 +10,5 @@ test_that("correct output", {
   expect_equal(finder_typo$City, "Exeter")
   expect_equal(finder_typo$IATA, "EXT")
   expect_equal(finder_port$Name, "Las Heras Airport")
-  expect_identical(class(airport_finder(city = "Nairobi")), c("tbl_df", "tbl", "data.frame"))
+  expect_identical(length(airport_finder(city = "Nairobi")), 4)
 })
