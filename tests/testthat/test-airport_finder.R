6ed677a6df7a9a8309d2fe05ff9f68cc561e47c3
lilyclements <lily@idems.international>
2024-05-01 13:02:00 +0100

    expect_identical to expect_equal

--- tests/testthat/test-airport_finder.R
+++ tests/testthat/test-airport_finder.R
@@ -10,5 +10,5 @@ test_that("correct output", {
   expect_equal(finder_typo$City, "Exeter")
   expect_equal(finder_typo$IATA, "EXT")
   expect_equal(finder_port$Name, "Las Heras Airport")
-  expect_identical(length(airport_finder(city = "Nairobi")), 4)
+  expect_equal(class(airport_finder(city = "Nairobi")), c("tbl_df", "tbl", "data.frame"))
 })
