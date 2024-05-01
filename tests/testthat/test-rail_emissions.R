dba63281159044b7fbc403393b754d6e6960694b
lilyclements <lily@idems.international>
2024-05-01 12:36:02 +0100

    rail_emissions test update

--- tests/testthat/test-rail_emissions.R
+++ tests/testthat/test-rail_emissions.R
@@ -6,6 +6,10 @@ test_that("correct calculations when you edit options on times and round trip", 
   expect_equal(4*rail_emissions("Southampton Central", "Manchester Piccadilly"), rail_emissions("Southampton Central", "Manchester Piccadilly", round_trip = TRUE, times_journey = 2))
 })
 
+test_that("Adding a via puts it in at the correct point (centre of journey, not end)", {
+  expect_equal(rail_emissions("Southampton Central", "Manchester Piccadilly", "Reading"), c(rail_emissions("Southampton Central", "Reading") + rail_emissions("Reading", "Manchester Piccadilly")))
+})
+
 test_that("incorrect values gives error", {
   expect_error(rail_emissions(from = "Southampton", to = "Manchester Piccadilly"))
   expect_error(rail_emissions(from = "Southampton Central", to = "manchester piccadilly"))
